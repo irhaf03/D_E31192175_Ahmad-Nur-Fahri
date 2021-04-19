136a2bfd967100c4118698c63b8285c90a41803f
irhaf03 <ririfah11@gmail.com>
2021-04-19 09:30:50 +0700

    minggu11

---
+++ minggu11/ci/application/views/Admin/_Template/content.php
@@ -0,0 +1,10 @@
+ <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+              <?= @$konten; ?>
+
+                </div>
+                <!-- /.container-fluid -->
+
+           
